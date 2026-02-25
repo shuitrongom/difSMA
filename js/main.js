aefd9175d512037ba2de7d5c8696b981da047956
sergiohuitron <sergio.huitron@isol.dev>
2026-02-24 23:58:38 -0600

    Se suben cambios de precidencia

--- js/main.js
+++ js/main.js
@@ -14,6 +14,19 @@
     
     // Initiate the wowjs
     new WOW().init();
+
+    // Carga imagen modal
+    var imageModal = document.getElementById('imageModal');
+imageModal.addEventListener('show.bs.modal', function (event) {
+  // Botón que activó el modal
+  var button = event.relatedTarget;
+  // Extraer información de los atributos data-
+  var imgSource = button.getAttribute('src');
+  
+  // Actualizar la imagen del modal
+  var modalImg = imageModal.querySelector('#imagePreview');
+  modalImg.src = imgSource;
+});
     
     
    // Back to top button
@@ -34,7 +47,38 @@
 
     $(".testimonial-carousel").owlCarousel({
         autoplay: true,
-        smartSpeed: 1500,
+        smartSpeed: 1000,
+        autoplayTimeout: 2500,
+        center: true,
+        dots: true,
+        loop: true,
+        margin: 50,
+        responsiveClass: true,
+        responsive: {
+            0:{
+                items:1
+            },
+            576:{
+                items:1
+            },
+            768:{
+                items:2
+            },
+            992:{
+                items:2
+            },
+            1200:{
+                items:3
+            }
+        }
+    });
+
+    // Noticias carousel
+
+    $(".notice-carousel").owlCarousel({
+        autoplay: true,
+        smartSpeed: 1000,
+        autoplayTimeout: 5000,
         center: true,
         dots: true,
         loop: true,
